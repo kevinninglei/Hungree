ffb2bf887077b8929a6e3f3251d94013408b5ba9
Annie Cook <anniecook@ostby.com>
2015-07-27 17:18:54 -0400

    make order model

---
+++ server/db/models/order.js
@@ -0,0 +1,29 @@
+'use strict';
+var crypto = require('crypto');
+var mongoose = require('mongoose');
+
+var orderSchema = new mongoose.Schema({
+	//make a static to fill if the user is a guest. add cookies to keep track of guest sessions
+	userId: {type: mongoose.Schema.ObjectId, ref: 'User'},
+	//array of dishes with quantities 
+	dishes: [{
+		dishId: {type: mongoose.Schema.ObjectId, ref: 'Dish'},
+		quantity: Number
+	}]
+	//make static to calculate total for checkout
+	total: { type: Number, default: 0}
+
+})
+
+orderSchema.pre('save', function (next) {
+	//mary need to try ['price']
+	Dish.find({}).populate('dishes.dishId', 'price').exec()
+	.then(function (price) {
+		this.total += price * this.dishes.quantity;
+	})
+	.then(null, function (err){
+		throw err.message
+	})
+})
+
+mongoose.model('Order', orderSchema);
