0ffbdd9115e038b3af6e6bb0c07937fae798cba1
Justin Kim <jkim430@gmail.com>
2015-07-28 14:51:09 -0400

    working seed file, added name field to user, commented out order pre save

--- seed.js
+++ seed.js
@@ -19,7 +19,7 @@ Refer to the q documentation for why and how q.invoke is used.
 
 */
 
-var bluebird = require('bluebird');
+// var bluebird = require('bluebird');
 var mongoose = require('mongoose');
 var connectToDb = require('./server/db');
 var User = mongoose.model('User');
@@ -29,11 +29,7 @@ var Order = mongoose.model('Order');
 var Review = mongoose.model('Review');
 var Tag = mongoose.model('Tag');
 var q = require('q');
-// var chalk = require('chalk');
 
-// var getCurrentUserData = function () {
-//     return q.ninvoke(User, 'find', {});
-// };
 
 var addresses = [
     new Address({street: '123 Sesame Street', city: 'Boston', state: 'MA', zip: 02144}),
@@ -70,108 +66,78 @@ var dishes = [
 ];
 
 var orders = [ //add userIds
-    new Order({ dishes: [{dishId: dishes[0], quantity: 1}, {dishId: dishes[1], quantity: 2}]}),
-    new Order({ dishes: [{dishId: dishes[2], quantity: 4}, {dishId: dishes[3], quantity: 1}]}),
-    new Order({ dishes: [{dishId: dishes[1], quantity: 3}, {dishId: dishes[2], quantity: 5}]})
+    new Order({dishes: [{dishId: dishes[0], quantity: 1}, {dishId: dishes[1], quantity: 2}]}),
+    new Order({dishes: [{dishId: dishes[2], quantity: 4}, {dishId: dishes[3], quantity: 1}]}),
+    new Order({dishes: [{dishId: dishes[1], quantity: 3}, {dishId: dishes[2], quantity: 5}]})
 ];
 
-
-// var seedUsers = function () {
-
-    var users = [
-        new User({
-            name: {first: 'KevinJustin', last: 'AnnieYves'},
-            email: 'testing@fsa.com',
-            password: 'password',
-            facebook: null,
-            google: null,
-            admin: false,
-            shippingAddress: addresses[0],
-            billing: {billingAddress: addresses[1], number: 3245558323493406, ccv: 445, exp: {month: 10, year: 2015}, cardType: 'Visa'},
-            dishes: [dishes[0], dishes[1]],
-            favorites: [dishes[3], dishes[1]],
-            orders: [orders[0]],
-            reviews: [reviews[0], reviews[1]]
-        }),
-        new User({
-            name: {first: 'Barack', last: 'Obama'},
-            email: 'obama@gmail.com',
-            password: 'potus',
-            facebook: null,
-            google: null,
-            admin: true,
-            shippingAddress: addresses[2],
-            billing: {billingAddress: addresses[3], number: 6453965834530596, ccv: 997, exp: {month: 09, year: 2015}, cardType: 'MasterCard'},
-            dishes: [dishes[2], dishes[3]],
-            favorites: [dishes[0], dishes[1]],
-            orders: [orders[1], orders[2]],
-            reviews: [reviews[2]]
-        })
-    ];
+var users = [
+    {
+        name: {first: 'KevinJustin', last: 'AnnieYves'},
+        email: 'testing@fsa.com',
+        password: 'password',
+        facebook: null,
+        google: null,
+        admin: false,
+        shippingAddress: addresses[0],
+        billing: {billingAddress: addresses[1], number: 3245558323493406, ccv: 445, exp: {month: 10, year: 2015}, cardType: 'Visa'},
+        dishes: [dishes[0], dishes[1]],
+        favorites: [dishes[3], dishes[1]],
+        orders: [orders[0]],
+        reviews: [reviews[0], reviews[1]]
+    },
+    {
+        name: {first: 'Barack', last: 'Obama'},
+        email: 'obama@gmail.com',
+        password: 'potus',
+        facebook: null,
+        google: null,
+        admin: true,
+        shippingAddress: addresses[2],
+        billing: {billingAddress: addresses[3], number: 6453965834530596, ccv: 997, exp: {month: 09, year: 2015}, cardType: 'MasterCard'},
+        dishes: [dishes[2], dishes[3]],
+        favorites: [dishes[0], dishes[1]],
+        orders: [orders[1], orders[2]],
+        reviews: [reviews[2]]
+    }
+];
 
 orders[0].userId = users[0];
 orders[1].userId = users[1];
 orders[2].userId = users[1];
 
 
-//     return q.invoke(User, 'create', users);
-
-// };
-
-// var seedStuff = function(name, stuff) {
-//     return q.invoke(name, 'create', stuff);
-// }
-
-// connectToDb.then(function () {
-//     q.all([seedStuff(Address, addresses), seedStuff(Tag, tags), seedStuff(Review, reviews), seedStuff(Dish, dishes), seedStuff(Order, orders)])
-//     .then(function() {
-//         return getCurrentUserData
-//     })
-//     .then(function (users) {
-//         if (users.length === 0) {
-//             return seedStuff(User, users);
-//             // return seedUsers();
-//         } else {
-//             console.log(chalk.magenta('Seems to already be user data, exiting!'));
-//             process.kill(0);
-//         }
-//     }).then(function () {
-//         console.log(chalk.green('Seed successful!'));
-//         process.kill(0);
-//     }).catch(function (err) {
-//         console.error(err);
-//         process.kill(1);
-//     });
-// });
-
+var models = [User, Address, Dish, Order, Review, Tag];
 
 var wipeDB = function() {
-
-    var models = [User, Address, Dish, Order, Review, Tag];
-
     models.forEach(function(model) {
         model.find({}).remove(function() {});
     });
 
-    return bluebird.resolve();
-
+    return q.resolve();
 };
 
 var seed = function() {
-    var notUser = [addresses, dishes, orders, reviews, tags];
-
-    notUser.forEach(function(arr) {
-        // console.log(arr);
-        arr.forEach(function(entry) {
-            console.log('entry', entry);
-            entry.save(function(e){
-                // console.log(e);
-            });
+    q.all(
+        models[0].create(users, function(err) {
+            if (err) console.error(err);
+        }),
+        models[1].create(addresses, function(err) {
+            if (err) console.error(err);
+        }),
+        models[2].create(dishes, function(err) {
+            if (err) console.error(err);
+        }),
+        models[3].create(orders, function(err) {
+            if (err) console.error(err);
+        }),
+        models[4].create(reviews, function(err) {
+            if (err) console.error(err);
+        }),
+        models[5].create(tags, function(err) {
+            if (err) console.error(err);
         })
-    })
-
-    // console.log('working')
-    User.create(users).exec()
+    )
     .then(function() {
         console.log('Database seeded!');
         process.kill(0);
@@ -179,32 +145,6 @@ var seed = function() {
     .then(null, function(err) {
         console.error(err);
     })
-
-    // bluebird.all(
-    //     MP.Day.create(days, function(err) {
-    //         if (err) {
-    //             console.error(err);
-    //         }
-    //     }),
-
-    //     MP.Entry.create(entries, function(err) {
-    //         if (err) {
-    //             console.error(err);
-    //         }
-    //     }),
-
-    //     MP.Recipe.create(recipes, function(err) {
-    //         if (err) {
-    //             console.error(err);
-    //         }
-    //     }))
-    //     .then(function() {
-    //         console.log('Database seeded!');
-    //         process.kill(0);
-    //     })
-    //     .then(null, function(err) {
-    //         console.error(err);
-    //     })
 };
 
 connectToDb.then(function() {
