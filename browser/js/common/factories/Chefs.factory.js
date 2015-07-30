c6faae5a3a1ce689f81ef2e814f6b23006cc5888
Justin Kim <jkim430@gmail.com>
2015-07-30 12:53:13 -0400

    cleaned up chefs factory and user routes

--- browser/js/common/factories/Chefs.factory.js
+++ browser/js/common/factories/Chefs.factory.js
@@ -1,11 +1,11 @@
 app.factory('Chefs',function($http){
   return{
-    getAll: function(){
-      return $http.get('/api/users/')
-        .then(function (res) {
+    getChefs: function() { //returns array of chef objects with dishes array
+            return $http.get('/api/users/chefs')
+            .then(function(res) {
                 return res.data;
             });
-    },
+        },
     getOne: function(id) {
     	return $http.get('/api/users/' + id)
     		.then(function (res) {
--- browser/js/list-dishes/list-dishes.js
+++ browser/js/list-dishes/list-dishes.js
@@ -2,15 +2,17 @@ app.config(function ($stateProvider) {
 
     $stateProvider.state('listDishes', {
         url: '/dishes',
-        template: `<p>test</p>
-        		<ul>
-        			<li ng-repeat="dish in nearbyDishes">{{dish}}</li>
-        		</ul>`,
+        template: `<div ng-repeat="chef in nearbyChefs">
+	        		<h3>{{chef.name.first}} {{chef.name.last}}</h3>
+	        		<ul>
+	        			<li ng-repeat="dish in chef.dishes">{{dish.name}}</li>
+	        		</ul>
+        		</div>`,
         controller: 'DishesCtrl'
     });
 
 });
 
-app.controller('DishesCtrl', function($scope, Dishes) {
-	$scope.nearbyDishes = Dishes.nearbyDishes;
+app.controller('DishesCtrl', function($scope, Chefs) {
+	$scope.nearbyChefs = Chefs.nearbyChefs 
 });
