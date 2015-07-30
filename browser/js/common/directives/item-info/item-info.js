c23eefeb9bdf24ed7bb711bb0d7da736cb22ef5c
Annie Cook <anniecook@ostby.com>
2015-07-30 16:04:42 -0400

    add dish factory

--- browser/js/common/directives/item-info/item-info.js
+++ browser/js/common/directives/item-info/item-info.js
@@ -3,8 +3,17 @@ app.directive('itemInfo', function () {
     return {
         restrict: 'E',
         templateUrl: 'js/common/directives/item-info/item-info.html',
-        link: function (scope, element) {
-            scope.item = Chefs.getOne(element._id);
+        link: function (scope, element, attr) {
+        	console.log("attr",attr);
+        	console.log("elem",element);
+        	var factory;
+        	if(attr.class === 'chef') {
+        		factory = 'Chef';
+        	} else if (attr.class === 'dish'){
+        		factory = 'Dish';
+        	}
+        	console.log(factory)
+          scope.item = factory.getOne(element._id);
         }
     };
 
