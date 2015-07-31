f52df76d62fe196cbe0a1b76037655dd25fe5149
Yves Yuen <yves.j.yuen@gmail.com>
2015-07-31 11:31:48 -0400

    added payment modal html and basic controller

--- browser/js/cart/cart.controller.js
+++ browser/js/cart/cart.controller.js
@@ -1,4 +1,4 @@
-app.controller('CartCtrl', function ($scope, $http, CartFactory){
+app.controller('CartCtrl', function ($scope, $http, CartFactory,$modal, $log){
 	$scope.total = 49;
 	//1. given an existing 'order', populate the cart
 	//2. ability to easy add dishes to the current order
@@ -9,8 +9,7 @@ app.controller('CartCtrl', function ($scope, $http, CartFactory){
 	$scope.updateSelectedCartItem = function(){
 		console.log('deleting button showing...');
 		$scope.showDeleteItemsButton = true;
-
-	}
+	};
 
 	var populateCart = function(order){
 		var currCart = [];
@@ -21,7 +20,7 @@ app.controller('CartCtrl', function ($scope, $http, CartFactory){
 				dishDescription: dish.dishId.description,
 				//price of a single dish
 				dishPrice: dish.dishId.price,
-				//used to configure the current <select> 
+				//used to configure the current <select>
 				quantity: dish.quantity,
 				isSelected: false
 			});
@@ -29,18 +28,42 @@ app.controller('CartCtrl', function ($scope, $http, CartFactory){
 		$scope.cart = currCart;
 		$scope.totalPrice = order.total;
 		console.log(currCart);
-	}
+	};
 
 	$scope.showDeleteItemsButton = false;
 
 	$scope.deleteItems = function(){
 		console.log($scope.cart);
-	}
+	};
 
 	CartFactory.getCurrentCart()
 		.then(function(order){
 			populateCart(order);
-		})
+		});
 
-	
-})
+  $scope.open = function () {
+    var modalInstance = $modal.open({
+      animation: true,
+      templateUrl: 'js/cart/payment/payment.html',
+      controller: 'paymentCtrl',
+      resolve: {
+        price: function () {
+          return $scope.totalPrice;
+        }
+      }
+    });
+
+    modalInstance.result.then(function (selectedItem) {
+      $scope.selected = selectedItem;
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+
+  $scope.toggleAnimation = function () {
+    $scope.animationsEnabled = !$scope.animationsEnabled;
+  };
+
+
+
+});
