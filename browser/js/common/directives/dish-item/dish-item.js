b4712f3c67bef91e32bb2241bafaa2d60e15d9a2
Annie Cook <anniecook@ostby.com>
2015-07-31 10:57:32 -0400

    made a dish-item directive that gets repeated over for the dishes list on chef page

---
+++ browser/js/common/directives/dish-item/dish-item.js
@@ -0,0 +1,11 @@
+app.directive('dishItem', function() {
+  return {
+    restrict: 'E',
+    scope: {
+      dish: "=dish",
+    },
+    templateUrl: 'js/common/directives/dish-item/dish-item.html',
+    link: function(scope, element, attribute) {
+    }
+  };
+});
