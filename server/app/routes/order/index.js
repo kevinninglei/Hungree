b7fb9a8c175c5e5a8ce114a2d950692e61a795f0
Yves Yuen <yves.j.yuen@gmail.com>
2015-07-30 20:21:24 -0400

    fixed order model (userid -> user)

--- server/app/routes/order/index.js
+++ server/app/routes/order/index.js
@@ -3,20 +3,20 @@ var mongoose = require('mongoose');
 var Order = mongoose.model('Order');
 var router = require('express').Router();
 
-router.get('/', function (req, res, next) {
+router.get('/', function(req, res, next) {
 	Order.find({}).populate('dishes.dishId').exec()
 		.then(function(orders) {
 			res.json(orders)
 		})
 		.then(null, next);
-})
+});
 
-router.post('/', function (req, res, next) {
+router.post('/', function(req, res, next) {
 	Order.create(req.body)
-		.then(function (order) {
+		.then(function(order) {
 			res.json(order);
 		})
 		.then(null, next);
-})
+});
 
 module.exports = router;
--- server/db/models/order.js
+++ server/db/models/order.js
@@ -7,39 +7,48 @@ var q = require('q');
 
 var orderSchema = new mongoose.Schema({
 	//make a static to fill if the user is a guest. add cookies to keep track of guest sessions
-	userId: {type: mongoose.Schema.ObjectId, ref: 'User'},
-	//array of dishes with quantities 
+	users: {
+		type: mongoose.Schema.ObjectId,
+		ref: 'User'
+	},
+	//array of dishes with quantities
 	dishes: [{
-		dishId: {type: mongoose.Schema.ObjectId, ref: 'Dish'},
+		dishId: {
+			type: mongoose.Schema.ObjectId,
+			ref: 'Dish'
+		},
 		quantity: Number
 	}],
 	//make static to calculate total for checkout
-	total: { type: Number, default: 0}
+	total: {
+		type: Number,
+		default: 0
+	}
 
-})
+});
 
 
-orderSchema.pre('save', function (next) {
+orderSchema.pre('save', function(next) {
 	var order = this;
 	//creates an array of promises for q to process
-	var promiseArr = this.dishes.map(function(dishInOrder){
+	var promiseArr = this.dishes.map(function(dishInOrder) {
 		return Dish.findById(dishInOrder.dishId).exec();
-	})
+	});
 
 	//once array of promises are resolved, the results is an
 	//array of dishes which must be computed.
-	q.all(promiseArr).then(function(dishes){
+	q.all(promiseArr).then(function(dishes) {
 		var currTotal = 0;
-		dishes.forEach(function(dish){
-			order.dishes.forEach(function (dishInOrder) {
+		dishes.forEach(function(dish) {
+			order.dishes.forEach(function(dishInOrder) {
 				if (String(dishInOrder.dishId) === String(dish._id)) {
 					currTotal += dishInOrder.quantity * dish.price;
 				}
-			})
-		})
+			});
+		});
 		order.total = currTotal;
 		next();
-	})
-})
+	});
+});
 
 mongoose.model('Order', orderSchema);
