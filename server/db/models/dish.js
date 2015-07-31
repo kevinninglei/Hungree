f587169463f42f67e3f0d795d3fba86d470ed65a
Justin Kim <jkim430@gmail.com>
2015-07-31 12:40:31 -0400

    single dish view

--- server/db/models/dish.js
+++ server/db/models/dish.js
@@ -61,10 +61,16 @@ dishSchema.statics.findDish = function(id){
         });
 }
 
-dishSchema.virtual('rating').get(function() { //average rating
-    return this.reviews.reduce(function(a,b) {
-        return a.rating+b.rating;
-    }) / this.reviews.length
-})
+// dishSchema.virtual('rating').get(function() { //average rating
+//     this.populate('reviews').exec()
+//     .then(function(dish) {
+        
+//     })
+//     return (this.reviews.reduce(function(startVal, review) {
+//         return startVal+review.rating
+//     }, 0) / this.reviews.length)
+// })
+
+// dishSchema.set('toJSON', {virtuals: true});
 
 mongoose.model('Dish', dishSchema);
