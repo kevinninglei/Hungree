136c9b168882443d9b26023ff75de1d7ddc9e723
Yves Yuen <yves.j.yuen@gmail.com>
2015-07-31 18:39:12 -0400

    added new logo

--- browser/js/cart/payment/payment.controller.js
+++ browser/js/cart/payment/payment.controller.js
@@ -1,16 +1,45 @@
-app.controller('paymentCtrl', function ($scope, $modalInstance) {
-
+app.controller('paymentCtrl', function($scope, $modalInstance, price) {
   $scope.price = price;
-  console.log($scope.price);
-  // $scope.selected = {
-  //   item: $scope.items[0]
-  // };
 
-  $scope.ok = function () {
-    $modalInstance.close($scope.selected.item);
+  $scope.setFormScope = function(scope) {
+    this.$form = $("#form-payment");
+
+    console.log(this.$form);
   };
 
-  $scope.cancel = function () {
+  $scope.stripeResponseHandler = function(status, response) {
+    console.log(status, response);
+    if (response.error) {
+      $scope.error = response.error.message;
+      // Show the errors on the form
+      // $form.find('.payment-errors').text(response.error.message);
+      // $form.find('button').prop('disabled', false);
+    } else {
+      // response contains id and card, which contains additional card details
+      var token = response.id;
+      // Insert the token into the form so it gets submitted to the server
+      $scope.form.append($('<input type="hidden" name="stripeToken" />').val(token));
+      // and submit
+      $scope.form.get(0).submit();
+    }
+
+  };
+
+  $scope.ok = function() {
+    Stripe.setPublishableKey('pk_test_ZJ2EdpJbGoQokkcIwkci5gOY');
+    Stripe.card.createToken(
+      // $scope.$form
+    {
+        number: '4242424242424242',
+        cvc: '123',
+        exp_month: '08',
+        exp_year: '2016'
+      }
+      , $scope.stripeResponseHandler);
+    return false;
+  };
+
+  $scope.cancel = function() {
     $modalInstance.dismiss('cancel');
   };
 });
