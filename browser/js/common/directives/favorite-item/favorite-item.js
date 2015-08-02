321bdad92d2b73f51ada0bd1418142b4630fdc9b
Yves Yuen <yves.j.yuen@gmail.com>
2015-08-02 18:48:46 -0400

    Added stars on one-chef/dish-item and favorite-item

--- browser/js/common/directives/favorite-item/favorite-item.js
+++ browser/js/common/directives/favorite-item/favorite-item.js
@@ -6,10 +6,19 @@ app.directive('favoriteItem', function($state, Chefs) {
     },
     templateUrl: 'js/common/directives/favorite-item/favorite-item.html',
     link: function(scope, elem, attr) {
-    	scope.goToDish = function(dish) {
-    		Chefs.viewDish = dish;
-      		$state.go('oneDish', {id: dish._id})
-      	}
+      scope.goToDish = function(dish) {
+        Chefs.viewDish = dish;
+        $state.go('oneDish', {
+          id: dish._id
+        })
+      };
+
+      scope.getNumber = function(num) {
+        return new Array(Math.round(num))
+      };
+      scope.getNumberInverse = function(num) {
+        return new Array(5 - Math.round(num));
+      };
     }
   };
 });
