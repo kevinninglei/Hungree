65c9c731fc600910ac1f6b43a3ad352a102b37de
Yves Yuen <yves.j.yuen@gmail.com>
2015-08-03 17:09:59 -0400

    completed my purchases page layout

--- browser/js/account/account.controller.js
+++ browser/js/account/account.controller.js
@@ -1,6 +1,6 @@
 app.controller('AccountCtrl', function($scope,chef,dishes,reviews,orders,favorites) {
    $scope.user = chef;
-   $scope.dishes = dishes;
+   $scope.myDishes = dishes;
    $scope.reviews = reviews;
    $scope.orders = orders;
    $scope.favorites = favorites;
