fa3c2f0da0a83ebdbf2468dcea99d81582d81d98
Yves Yuen <yves.j.yuen@gmail.com>
2015-08-03 20:04:50 -0400

    Added toggle on and off for long descriptions in orders and cart

--- browser/js/account/account.controller.js
+++ browser/js/account/account.controller.js
@@ -4,9 +4,14 @@ app.controller('AccountCtrl', function($scope,chef,dishes,reviews,orders,favorit
    $scope.reviews = reviews;
    $scope.orders = orders;
    $scope.favorites = favorites;
-
+   $scope.limit = 100;
+   $scope.toggled = true;
+//Need to add in directive
   $scope.addToOrder = function(dish) {
     CartFactory.cartOrders.push(dish);
     CartFactory.addToCart(dish, 1);
+  };
+  $scope.toggleComment = function(){
+    $scope.toggled = !$scope.toggled;
   };
 });
