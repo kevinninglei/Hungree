536bdfad25e52d7cd8e8213d73f0fa2559106161
Kevin Lei <knl35@cornell.edu>
2015-08-04 16:32:58 -0400

    Accept and Reject orders

--- browser/js/account/account.state.js
+++ browser/js/account/account.state.js
@@ -19,7 +19,10 @@ app.config(function($stateProvider) {
         },
         favorites: function(Favorites, $stateParams) {
           return Favorites.getFavsForUser($stateParams.id);
-        }
+        },
+        receivedOrders: function(Orders, $stateParams) {
+          return Orders.getReceivedOrders($stateParams.id);
+        },
 
         //not working dont know why
         // currentUser: function (UserFactory, $stateParams) {
@@ -28,32 +31,37 @@ app.config(function($stateProvider) {
       }
     })
     .state('account.settings', {
-      controller: 'AccountCtrl',
+      // controller: 'AccountCtrl',
       url: '/settings',
       templateUrl: 'js/account/account.settings.html'
     })
     .state('account.overview', {
-      controller: 'AccountCtrl',
+      // controller: 'AccountCtrl',
       url: '/overview',
       templateUrl: 'js/account/account.overview.html'
     })
     .state('account.dishes', {
-      controller: 'AccountCtrl',
+      // controller: 'AccountCtrl',
       url: '/dishes',
       templateUrl: 'js/account/account.dishes.html'
     })
+    .state('account.chefdashboard', {
+      // controller: 'AccountCtrl',
+      url: '/chefdashboard',
+      templateUrl:'js/account/account.chefdashboard.html'
+    })
     .state('account.favorites', {
-      controller: 'AccountCtrl',
+      // controller: 'AccountCtrl',
       url: '/favorites',
       templateUrl: 'js/account/account.favorites.html'
     })
     .state('account.purchases', {
-      controller: 'AccountCtrl',
+      // controller: 'AccountCtrl',
       url: '/purchases',
       templateUrl: 'js/account/account.purchases.html'
     })
     .state('account.reviews', {
-      controller: 'AccountCtrl',
+      // controller: 'AccountCtrl',
       url: '/reviews',
       templateUrl: 'js/account/account.reviews.html'
     });
--- browser/js/common/factories/Orders.factory.js
+++ browser/js/common/factories/Orders.factory.js
@@ -12,9 +12,35 @@ app.factory('Orders', function($http) {
         .then(function(orders) {
           return orders.map(function(order) {
             return order.dishes;
-
           });
         })
+    },
+    getReceivedOrders: function(id) {
+      return $http.get(`/api/users/${id}/receivedorders`)
+        .then(function(res) {
+
+          //filter dishes that do not correspond with the chef
+          res.data.forEach(function(order){
+            order.dishes = order.dishes.filter(function(dish){
+              return (String(dish.dishId.user) === id);
+            })
+          })
+          return res.data;
+        });
+    },
+    updateDishOrder: function (id, order, dish, status){
+      var payload = {
+        order: order,
+        dish: dish,
+        status: status
+      }
+      return $http.put(`/api/users/${id}/receivedorders/update`, payload)
+        .then(function(order) {
+          order.data.dishes = order.data.dishes.filter(function(dish){
+              return (String(dish.dishId.user) === id);
+          })
+          return order.data;
+        });
     }
   };
 });
