849d42523a42c3093dad1f31f835bf3f49208e6a
Joe Alves <joe@fullstackacademy.com>
2015-08-04 10:52:09 -0400

    Remove

--- browser/js/one-chef/one-chef.controller.js
+++
@@ -1,8 +0,0 @@
-app.controller('OneChefCtrl', function ($scope, chef, dishes, reviews, favorites,orders) {
-	$scope.chef = chef;
-	$scope.dishes = dishes;
-	$scope.reviews = reviews;
-	$scope.favorites = favorites;
-  	$scope.orders = orders;
-});
-
--- browser/js/signup/signup.state.js
+++
@@ -1,9 +0,0 @@
-app.config(function ($stateProvider) {
-
-    $stateProvider.state('signUp', {
-        url: '/signup',
-        templateUrl: 'js/signup/signup.html',
-        controller: 'SignupCtrl'
-    });
-
-});
